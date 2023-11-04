861c1bcee852831a95b84d5a9b57805fda820946
Huz <huzaifa.tatari@Gmail.com>
2023-11-04 16:53:26 +0500

    Adding some functionality Adding how to check which mouse button was pressed

--- MouseEvents.py
+++ MouseEvents.py
@@ -1,8 +1,6 @@
-import typing
-from PyQt5 import QtGui
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+from PyQt6.QtWidgets import QMainWindow, QLabel, QApplication
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import QSize, Qt, QTimer
 import ctypes
 
 # This code makes it so windows doesn't think Pythonw.exe's icon should be used for this window's icon
@@ -30,20 +28,42 @@ class MainWindow(QMainWindow):                              #<- Boilerplate
         
         self.setCentralWidget(self.label)
 
+    def appendText(self, textToAppend):
+        
+        currText = self.label.text()
+        self.label.setText("\n".join([currText, textToAppend]))
 
     def mouseMoveEvent(self, e):
         self.label.setText("Oooo moving your mouse...\nnaughty naughty...")
         self.label.setStyleSheet("color:blue")
 
-        if self.timer.isActive():               #deactivating the timer if mouse is not idle
+        if self.timer.isActive():                   #deactivating the timer if mouse is not idle
             self.timer.stop()
+
+        if e.button() == Qt.MouseButton.LeftButton:
+            self.appendText("Left Button Pressed")
+        elif e.button() == Qt.MouseButton.RightButton:
+            self.appendText("Right Button Pressed")
+        elif e.button() == Qt.MouseButton.MiddleButton:
+            self.appendText("Middle Button Pressed")
+
+        if e.button() == Qt.MouseButton.NoButton:
+            self.appendText("No Button Pressed")
 
     def mousePressEvent(self, e):
         self.label.setText("Hello i am pressing my mouse, here too much raining\nOOoooUUuuuOOoooo")
         self.label.setStyleSheet("color:purple")
 
-        if self.timer.isActive():               #deactivating the timer if mouse is not idle
+        if self.timer.isActive():                   #deactivating the timer if mouse is not idle
             self.timer.stop()
+
+        if e.button() == Qt.MouseButton.LeftButton:
+            self.appendText("Left Button Pressed")
+        elif e.button() == Qt.MouseButton.RightButton:
+            self.appendText("Right Button Pressed")
+        elif e.button() == Qt.MouseButton.MiddleButton:
+            self.appendText("Middle Button Pressed")
+        
 
     def mouseReleaseEvent(self, e):
         self.label.setText("UNHAND ME YOU FIEND")
@@ -53,12 +73,26 @@ class MainWindow(QMainWindow):                              #<- Boilerplate
         self.timer.timeout.connect(self.resetWindow)#(when the mouse is sitting idle)
         self.timer.start()
 
+        if e.button() == Qt.MouseButton.LeftButton:
+            self.appendText("Left Button Released")
+        elif e.button() == Qt.MouseButton.RightButton:
+            self.appendText("Right Button Released")
+        elif e.button() == Qt.MouseButton.MiddleButton:
+            self.appendText("Middle Button Released")
+
     def mouseDoubleClickEvent(self, e):
         self.label.setText("Oh my.. GAAAAAAHHHHHHH u press me 2 tems")
         self.label.setStyleSheet("color:green")
 
-        if self.timer.isActive():               #deactivating the timer if mouse is not idle
+        if self.timer.isActive():                   #deactivating the timer if mouse is not idle
             self.timer.stop()
+
+        if e.button() == Qt.MouseButton.LeftButton:
+            self.appendText("Left Button Pressed")
+        elif e.button() == Qt.MouseButton.RightButton:
+            self.appendText("Right Button Pressed")
+        elif e.button() == Qt.MouseButton.MiddleButton:
+            self.appendText("Middle Button Pressed")
 
     def resetWindow(self):
         self.label.setText("Click in this window")
@@ -71,4 +105,4 @@ app = QApplication([])  #initializing the application
 window = MainWindow()
 window.show()           #Windows are made invisible by default, need show func to show them
 
-app.exec_()             #executing the program
+app.exec()              #executing the program
