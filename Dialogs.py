0d7711d7fa52a555f460b07035bc4bc37eaaf6e6
Huz <huzaifa.tatari@Gmail.com>
2023-11-15 10:39:43 +0500

    using classes to make dialog box

--- Dialogs.py
+++ Dialogs.py
@@ -6,6 +6,24 @@ from ctypes import windll
 myappid = u'mycompany.myproduct.subproduct.version'
 windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
+class customDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("HELLO! IT'S A DIALOG WINDOW")
+
+        Qbtns = QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+
+        self.buttonBox = QDialogButtonBox(Qbtns)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        message = QLabel("uhhhh idk i think something happened")
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -23,9 +41,12 @@ class MainWindow(QMainWindow):
     def buttonClicked(self, s):
         print(s, "Button Pressed")
 
-        dlg = QDialog(self)
-        dlg.setWindowTitle("Dialog Window")
-        dlg.exec()
+        dlg = customDialog()
+        if dlg.exec():
+            print("It worked!")
+        else:
+            print("It didn't work :(")
+
 
 
 app = QApplication([])
